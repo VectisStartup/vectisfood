1645553b88c69f425340d126a282c1048ecf9d62
EfrainMario <efrainmario345@gmail.com>
2019-01-29 12:00:31 +0100

    hh

--- php/index.php
+++ php/index.php
@@ -1,13 +1,33 @@
-<!DOCTYPE html>
-<html lang="pt-PT">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <script type="module" src="./../js/index.js">
-    </script>
-    <script></script>
-</body>
-</html>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./../css/bootstrap.min.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+
+        <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                
+            </div>
+        </div>
+        </div>
+
+
+
+      <script type="module" src="./../js/index.js"></script>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="./../js/Libs/jquery-3.3.1.slim.min.js"></script>
+    <script src="./../js/libs/popper.min.js"></script>
+    <script src="./../js/libs/bootstrap.min.js"></script>
+  </body>
+</html>   
+    
