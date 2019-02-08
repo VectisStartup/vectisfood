af54c653bbc156ccf9be53ad53051b039a661303
cndidofernandes <36197523+cndidofernandes@users.noreply.github.com>
2019-02-08 15:11:57 +0100

    FCM modal

--- index.php
+++ index.php
@@ -356,7 +356,7 @@
 
 
         messaging.onMessage(function(payload) {
-            $('.modal').open();
+            $('.modal').Open();
             $('.modal h4').html(payload.notification.title);
             $('.modal p').html(payload.notification.body);
             console.log('Message received. ', payload);
